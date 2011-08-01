6dc3fcd51105f14b2e5ab0b5db65e4e4bbbddc7c
Jonathan Lister <jonathan@withjandj.com>
2011-08-01 14:49:29 +0100

    changing menus

--- wordpress/wp-content/themes/evo/functions.php
+++ wordpress/wp-content/themes/evo/functions.php
@@ -1,6 +1,2 @@
 <?php
-// Menus
-if ( function_exists( 'register_nav_menu' ) ) {
-	register_nav_menu( 'top_menu', 'Top Menu' );
-}
 ?>
--- wordpress/wp-content/themes/evo/snippets/top-menu.php
+++ wordpress/wp-content/themes/evo/snippets/top-menu.php
@@ -1,6 +1,6 @@
 <?php wp_nav_menu( array(
 	'container'		 => 'false',
-	'theme_location' => 'top_menu',
+	'theme_location' => 'main_menu',
 	'menu_id' => '',
 	'menu_class' => ''
 	)
