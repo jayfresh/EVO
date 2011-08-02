51250c814fff83b6e179a512ab7b1bc1c5f77971
Jonathan Lister <jonathan@withjandj.com>
2011-08-02 18:08:33 +0100

    tweaked index layout

--- wordpress/wp-content/themes/evo/index.php
+++ wordpress/wp-content/themes/evo/index.php
@@ -4,15 +4,15 @@ get_header();
 get_template_part('snippets/header'); ?>
 
 <div class="grid24col innerWrap">
-	<?php
-	get_template_part('snippets/left-hand-menu');
-	get_template_part('snippets/breadcrumbs'); ?>
-	
+	<?php get_template_part('snippets/breadcrumbs'); ?>
 	<div class="grid18col right">
-		<?php
-		get_template_part('loop');
-		?>
+	<?php
+	get_template_part('loop');
+	?>
 	</div>
+	<?php get_template_part('snippets/left-hand-menu'); ?>
+	
+
 	<br class="clearboth"/>
 </div>
 
