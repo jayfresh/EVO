e94101d3e74c378ff60466b3925d140a71af2047
joshuwar <josh.u.war@gmail.com>
2011-08-02 17:57:03 +0100

    adding loop stuff

---
+++ wordpress/wp-content/themes/evo/index.php
@@ -0,0 +1,22 @@
+<?php
+/* for content pages */
+get_header();
+get_template_part('snippets/header'); ?>
+
+<div class="grid24col innerWrap">
+	<?php
+	get_template_part('snippets/left-hand-menu'); 
+	get_template_part('snippets/breadcrumbs'); ?>
+	
+	<div class="article grid18col right">
+		<?php
+		get_template_part('loop');
+		?>
+	</div>
+	<br class="clearboth"/>
+</div>
+
+<?php
+get_template_part('snippets/footer');
+get_footer();
+?>
