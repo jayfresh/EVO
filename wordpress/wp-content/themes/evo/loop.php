87e07785c5ef035ac2cbab5e3c18e58328ed512b
joshuwar <josh.u.war@gmail.com>
2011-08-02 18:38:13 +0100

    adding static soc meds

--- wordpress/wp-content/themes/evo/loop.php
+++ wordpress/wp-content/themes/evo/loop.php
@@ -1,5 +1,11 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <div class="article">
+	<div class="grid3col sharing right">
+		<a id="facebook" href="#">Share on Facebook</a>
+		<a id="twitter" href="#">Share on Twitter</a>
+		<a id="email" href="#">Share by email</a>
+		<a id="print" href="#">Print this page</a>
+	</div>
 	<div class="grid13col">
 		<h1><?php the_title(); ?></h1>
 		<p class="large"><?php echo get_the_excerpt(); ?></p>
