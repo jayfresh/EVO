7a57a400682d73fda1f7fd247fe642ec98b81dee
Jonathan Lister <jonathan@withjandj.com>
2011-08-02 13:46:20 +0100

    added breadcrumbs and wired in with style. Added to single and page templates

--- wordpress/wp-content/themes/evo/page.php
+++ wordpress/wp-content/themes/evo/page.php
@@ -3,6 +3,7 @@
 get_header();
 get_template_part('snippets/header');
 get_template_part('snippets/left-hand-menu');
+get_template_part('snippets/breadcrumbs');
 get_template_part('loop');
 get_template_part('snippets/footer');
 get_footer();
