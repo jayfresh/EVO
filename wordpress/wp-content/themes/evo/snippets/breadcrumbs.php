27e12f037208a2ff45cb2cfa314db81da1597de6
joshuwar <josh.u.war@gmail.com>
2011-08-02 17:56:58 +0100

    adding loop stuff

--- wordpress/wp-content/themes/evo/snippets/breadcrumbs.php
+++ wordpress/wp-content/themes/evo/snippets/breadcrumbs.php
@@ -6,7 +6,7 @@ while($page_target->post_parent) {
 	$page_target = get_post($page_target->post_parent);
 	array_unshift($parent_pages,$page_target);
 } ?>
-<ul class="breadcrumb">
+<ul class="breadcrumb grid18col right">
 <?php foreach($parent_pages as $post) : ?>
 	<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
 <?php endforeach; ?>
--- wordpress/wp-content/themes/evo/snippets/left-hand-menu.php
+++ wordpress/wp-content/themes/evo/snippets/left-hand-menu.php
@@ -12,16 +12,18 @@ if($page_target->post_parent) {
 $page_title = get_the_title();
 $query = new WP_query("post_type=page&post_parent=".$page_target->ID."&orderby=menu_order&order=asc");
 if($query->have_posts()) : ?>
-	<ul class="sidebar margintop grid6col">
-		<li class="active"><a href="<?php echo get_permalink($page_target->ID); ?>"><?php echo get_the_title($page_target->ID); ?></a></li>
-		<?php while ( $query->have_posts() ) : $query->the_post();
-			$current_title = get_the_title();
-			$class="";
-			if($page_title==$current_title) {
-				$class = ' class="active"';
-			}
-		?>
-		<li<?php echo $class; ?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-		<?php endwhile; wp_reset_query(); ?>
-	</ul>
+	<div class="sidebarWrap grid6col">
+		<ul class="sidebar margintop">
+			<li class="active"><a href="<?php echo get_permalink($page_target->ID); ?>"><?php echo get_the_title($page_target->ID); ?></a></li>
+			<?php while ( $query->have_posts() ) : $query->the_post();
+				$current_title = get_the_title();
+				$class="";
+				if($page_title==$current_title) {
+					$class = ' class="active"';
+				}
+			?>
+			<li<?php echo $class; ?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+			<?php endwhile; wp_reset_query(); ?>
+		</ul>
+	</div>
 <?php endif; ?>
