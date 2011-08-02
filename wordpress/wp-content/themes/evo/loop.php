901ab56fceaec4843ef8a798225d9024cb2517dd
joshuwar <josh.u.war@gmail.com>
2011-08-02 18:30:55 +0100

    sorting article page styles

--- wordpress/wp-content/themes/evo/loop.php
+++ wordpress/wp-content/themes/evo/loop.php
@@ -1,7 +1,9 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <div class="article">
-	<h1><?php the_title(); ?></h1>
-	<p class="large"><?php echo get_the_excerpt(); ?></p>
-	<?php the_content(); ?>
+	<div class="grid13col">
+		<h1><?php the_title(); ?></h1>
+		<p class="large"><?php echo get_the_excerpt(); ?></p>
+		<?php the_content(); ?>
+	</div>
 </div>
 <?php endwhile; endif; ?>
