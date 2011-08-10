71b909796691c2f7c2c5ba2b2460b560724ff03b
Jonathan Lister <jonathan@withjandj.com>
2011-08-10 23:34:25 +0100

    added access key title to header logo

--- wordpress/wp-content/themes/evo/snippets/header.php
+++ wordpress/wp-content/themes/evo/snippets/header.php
@@ -2,7 +2,7 @@
 	<div class="grid24col png_bg">
 		<div id="header">
 			<a name="TOP"></a>
-			<div class="grid6col left"><a href="<?php bloginfo('url'); ?>" class="logo left"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo.png" alt="EVO" title="EVO logo" /></a></div>
+			<div class="grid6col left"><a href="<?php bloginfo('url'); ?>" class="logo left"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo.png" alt="EVO" title="Access key 1 - Home page" /></a></div>
 			<?php get_template_part('snippets/top-menu'); ?>
 			<?php get_template_part('snippets/search-box-menu'); ?>
 		</div><!-- end #header -->
