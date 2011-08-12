8334563b4bd3256c1b96809ea9fb8a6341d09088
joshuwar <josh.u.war@gmail.com>
2011-08-12 16:40:04 +0100

    altering news page styling

--- wordpress/wp-content/themes/evo/news-loop.php
+++ wordpress/wp-content/themes/evo/news-loop.php
@@ -10,9 +10,9 @@
 	</div>
 	<div class="grid13col">
 		<hr />
-		<?php the_date('d.m.y'); ?>
+		<p><?php echo get_the_date('d.m.y'); ?></p>
 		<?php if(!is_singular()) : ?>
-		<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+		<h2 class="small"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 		<?php else : ?>
 		<h2><?php the_title(); ?></h2>
 		<?php endif; ?>
