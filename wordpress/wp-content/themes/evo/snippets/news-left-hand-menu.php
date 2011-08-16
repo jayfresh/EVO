db4a48dcedaff9541f16156cdacfa96bcfa9af6d
joshuwar <josh.u.war@gmail.com>
2011-08-16 17:23:09 +0100

    twitter styling

--- wordpress/wp-content/themes/evo/snippets/news-left-hand-menu.php
+++ wordpress/wp-content/themes/evo/snippets/news-left-hand-menu.php
@@ -1,7 +1,7 @@
 <div class="sidebarWrap grid6col dottop padtop">
 	<div class="sidebar push4">
 		<h2 id="newsArchive">News Archive</h2>
-		<div class="box grey border">
+		<div id="twitterWidget" class="box grey border">
 			<?php if ( is_active_sidebar( 'news-sidebar' ) ) : ?>
 				<?php dynamic_sidebar( 'news-sidebar' ); ?>
 			<?php endif; ?>
