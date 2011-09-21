c7b72a0dd1c4848e39b11f14090a18f06e9c0f78
joshuwar <josh.u.war@gmail.com>
2011-09-20 17:25:32 -0700

    making magnifying glass in to a clickable button on inline search form

--- wordpress/wp-content/themes/evo/searchform.php
+++ wordpress/wp-content/themes/evo/searchform.php
@@ -1,3 +1,4 @@
 <form action="<?php bloginfo('url'); ?>" method="get">
 	<input id="inline_s" type="search" name="s" value="<?php echo $_REQUEST['s']; ?>"/>
+	<input id="inline_searchButton" type="submit" value="Search" />
 </form>
