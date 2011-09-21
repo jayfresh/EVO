86a4c62edd776b5e22354ac994a4f8aaada6b22e
Jonathan Lister <jonathan@withjandj.com>
2011-09-20 17:22:06 -0700

    fixing safari search box problem by making it a text input

--- wordpress/wp-content/themes/evo/searchform.php
+++ wordpress/wp-content/themes/evo/searchform.php
@@ -1,3 +1,3 @@
 <form action="<?php bloginfo('url'); ?>" method="get">
-	<input id="inline_s" type="search" name="s" value="<?php echo $_REQUEST['s']; ?>"/>
+	<input id="inline_s" type="text" name="s" value="<?php echo $_REQUEST['s']; ?>"/>
 </form>
