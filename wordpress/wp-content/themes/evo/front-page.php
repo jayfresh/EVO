95a45287051b283314a25aa87ecd59290e01907a
joshuwar <josh.u.war@gmail.com>
2011-10-04 14:01:06 -0700

    making home info links work like bigblocks

--- wordpress/wp-content/themes/evo/front-page.php
+++ wordpress/wp-content/themes/evo/front-page.php
@@ -61,7 +61,7 @@ global $more;
 		<!-- deliberately no whitespace between li elements -->
 		<ul id="homeLinks">
 			<?php while($child_pages->have_posts()) : $child_pages->the_post(); ?>
-			<li>
+			<li>RAAR
 				<?php the_post_thumbnail( $info_page, 'homeinfolinks' ); ?>
 				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 			</li><?php endwhile; ?>
