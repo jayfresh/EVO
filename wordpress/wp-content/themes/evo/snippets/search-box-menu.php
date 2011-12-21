ed3afc62f89bb9afa84924e2836e60f80fe5db3e
Jonathan Lister <jonathan@withjandj.com>
2011-12-21 16:48:17 +0000

    adding input->button image replacement fix

--- wordpress/wp-content/themes/evo/snippets/search-box-menu.php
+++ wordpress/wp-content/themes/evo/snippets/search-box-menu.php
@@ -1,7 +1,7 @@
 <div id="search" class="grid6col left box darkgrey marginleft">
 	<form action="<?php bloginfo('url'); ?>" method="get">
 		<input id="field" name="s" type="text" placeholder="Search" />
-		<input id="searchButton" type="submit" value="search" /> 
+		<button id="searchButton" type="submit">search</button>
 	</form>
 	<?php wp_nav_menu( array(
 		'container'		 => 'false',
