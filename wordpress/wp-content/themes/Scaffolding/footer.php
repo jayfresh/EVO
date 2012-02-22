d01e4a65b696373d461f94e90b45d1ac17585b26
Jonathan Lister <jonathan@withjandj.com>
2012-02-22 16:20:37 +0000

    removing selectivizr to see whether it conflicts with addthis

--- wordpress/wp-content/themes/Scaffolding/footer.php
+++ wordpress/wp-content/themes/Scaffolding/footer.php
@@ -1,7 +1,7 @@
 		<?php wp_footer(); ?>
 		<script type="text/javascript" src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/app.js"></script>
 		<!--[if (gte IE 6)&(lte IE 8)]>
-			<script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/selectivizr-min.js"></script>
+			<!--<script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/selectivizr-min.js"></script>-->
 		<![endif]-->
 		<!--[if lt IE 7 ]>
 			<script src="<?php bloginfo( 'template_url' ); ?>/js/dd_belatedpng.js"></script>
